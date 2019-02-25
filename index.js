06f886f865c4f40edc655303d2b027ac9b67a4c4
shaiksameer46 <shaiksameer46@outlook.com>
2019-02-25 20:44:01 +0530

    make it better

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var port = process.env.PORT || 3000;
 var app = express();
-app.get('/hi', function (req, res) {
+app.get('/', function (req, res) {
  res.send("heo world");
 });
+
 app.listen(port, function () {
  console.log("Example app listening on port !" + port);
 });
